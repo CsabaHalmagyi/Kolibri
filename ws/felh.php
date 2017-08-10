fdf2f0324a0f9feabc6599b412dc8331eed451ae
CsabaHalmagyi <csaba@ebi.ac.uk>
2017-08-10 18:31:19 +0100

    Cleanup

--- ws/felh.php
+++ ws/felh.php
@@ -14,7 +14,7 @@ function printResponse($errorCode, $message, $response){
 			'message' => $message,
 			'data' => $response
 	);
-	
+
 	header ( "Access-Control-Allow-Origin: *" );
 	header ( 'Content-Type: application/json; charset=utf-8');
 	echo json_encode ( $responseArray );
@@ -25,14 +25,17 @@ if (!isset($_SESSION['felhasznalo']['felhasznalo_id'])) printResponse(1000, "Log
 if ($_SESSION['jog']['admin'] != "1") printResponse(7, "Nincs jogosultságod a művelethez", null);
 
 if(count($_POST)>0){
-	
-	$action = $_POST['action'];	
-	
-	//ACTION: getUsers	
+
+	$action = $_POST['action'];
+
+	//ACTION: getUsers
 	if($action == "getUsers"){
-		
+
 		$dbh = connectToDB();
-		
+		if(!$dbh){
+			printResponse(2,"Adatbázis kapcsolat nem jött létre", null);
+		}
+			
 		$response = getUsersAndGroups($dbh);
 
 		if(count($response['users'])>0) {
@@ -47,38 +50,35 @@ if(count($_POST)>0){
 	//ACTION: getAllGroups
 	else if($action == "getAllGroups"){
 
-			$dbh = connectToDB();
-			if(!$dbh){
-				printResponse("2","Adatbázis kapcsolat nem jött létre", null);
-			}
+		$dbh = connectToDB();
+		if(!$dbh){
+			printResponse(2,"Adatbázis kapcsolat nem jött létre", null);
+		}
 			
-			$response = getUsersAndGroups($dbh);
+		$response = getUsersAndGroups($dbh);
 
-			$csoportok = $response['groups'];
+		$csoportok = $response['groups'];
 			
-			if(count(csoportok) != 0){
-				$errorCode = 9;
-				$message = 'Nincsenek csoportok definiálva!';
-				printResponse($errorCode,$message,null);
-			}
-			else{
-				$message = 'Sikeres csoport beolvasás.';
-				printResponse(0,$message,$csoportok);
-			}
-			
-			
+		if(count(csoportok) != 0){
+			$errorCode = 9;
+			$message = 'Nincsenek csoportok definiálva!';
+			printResponse($errorCode,$message,null);
+		}
+		else{
+			$message = 'Sikeres csoport beolvasás.';
+			printResponse(0,$message,$csoportok);
+		}
 
-		
 	}
 	//ACTION: createNewUser
 	else if($action == "createNewUser"){
-		
+
 		$group = $_POST['group'];
 		$userName = $_POST['username'];
 		$pass = $_POST['password'];
 		$firstName = $_POST['firstname'];
 		$surName = $_POST['surname'];
-	
+
 		if (empty($group) || empty($userName) || empty($pass) || empty($firstName) || empty($surName)){
 			$errorCode = 10;
 			$message = "Hiányzó felhasználói adat!";
@@ -88,17 +88,17 @@ if(count($_POST)>0){
 
 			$dbh = connectToDB();
 			if(!$dbh){
-				printResponse("2","Adatbázis kapcsolat nem jött létre", null);
+				printResponse(2,"Adatbázis kapcsolat nem jött létre", null);
 			}
-			
+				
 			$result = createNewUser($dbh, $userName, $pass, $firstName, $surName, $group);
-			printResponse($result['errorCode'], $result['message'], null);	
+			printResponse($result['errorCode'], $result['message'], null);
 		}
-	
+
 	}
 	//ACTION: deleteUser
 	else if($action == "deleteUser"){
-		
+
 		$userID = $_POST['userid'];
 
 		if (empty($userID)){
@@ -112,45 +112,45 @@ if(count($_POST)>0){
 			printResponse($errorCode,$message,null);
 		}
 		else{
-		
+
 			$dbh = connectToDB();
 			if(!$dbh){
-				printResponse("2","Adatbázis kapcsolat nem jött létre", null);
+				printResponse(2,"Adatbázis kapcsolat nem jött létre", null);
 			}
-			
+				
 			$result = deleteUser($dbh, $userID);
-			printResponse($result['errorCode'], $result['message'], null);	
+			printResponse($result['errorCode'], $result['message'], null);
 		}
 	}
 	//ACTION: updateUser
 	else if($action == "updateUser"){
-	
+
 		$userID = $_POST['userid'];
-	
+
 		if (empty($userID)){
 			$errorCode = 12;
 			$message = "Hiányzó felhasználó azonosító!";
 			printResponse($errorCode,$message,null);
 		}
 		else{
-	
+
 			$dbh = connectToDB();
 			if(!$dbh){
-				printResponse("2","Adatbázis kapcsolat nem jött létre", null);
+				printResponse(2,"Adatbázis kapcsolat nem jött létre", null);
 			}
-			
-			$result = updateUser($dbh, $userID, $_POST['firstname'], $_POST['surname'], 
+				
+			$result = updateUser($dbh, $userID, $_POST['firstname'], $_POST['surname'],
 			$_POST['group'], $_POST['status'], $_POST['pass']);
-			printResponse($result['errorCode'], $result['message'], null);	
-	
+			printResponse($result['errorCode'], $result['message'], null);
+
 		}
-	}	
-	
+	}
+
 	else{
 		$errorCode = 1;
 		$message = 'Ismeretlen feladat!';
 		printResponse($errorCode,$message,null);
-		
+
 	}
 }
 
